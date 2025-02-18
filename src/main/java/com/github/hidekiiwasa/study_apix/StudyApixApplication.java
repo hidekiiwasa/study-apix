d01cacbbdaefdc33fb3d471718a7f8757e415b9b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-17 22:44:49 -0300

    first commit

---
+++ src/main/java/com/github/hidekiiwasa/study_apix/StudyApixApplication.java
@@ -0,0 +1,13 @@
+package com.github.hidekiiwasa.study_apix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApixApplication.class, args);
+	}
+
+}
