cf6bc1120d01064da6483e04dbc2171e0e8d6699
hidekiiwasa <Spider321011@hotmail.com>
2025-04-07 22:53:33 -0300

    feat: get and delete methods

--- src/main/java/com/github/hidekiiwasa/study_apix/service/ProdutoService.java
+++ src/main/java/com/github/hidekiiwasa/study_apix/service/ProdutoService.java
@@ -1,5 +1,8 @@
 package com.github.hidekiiwasa.study_apix.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +22,22 @@ public class ProdutoService {
         produto.setNome(dto.getNome());
 
         return produtoRepository.save(produto);
+    }
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public boolean deleteById(Long id) {
+        if (produtoRepository.existsById(id)) {
+            produtoRepository.deleteById(id);
+            return true;
+        }
+        
+        return false;
     }
 }
